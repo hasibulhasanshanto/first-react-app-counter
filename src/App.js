93750dc923538f369c4f1b6d9897288016d1f67e
hasibulhasanshanto <kajal.shanto@gmail.com>
2022-09-20 00:23:25 +0600

    Added json place holder comments api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
       <Counter></Counter>
+      <Comments></Comments>
     </div>
   );
 }
@@ -36,4 +37,32 @@ function Counter() {
   );
 }
 
+function Comments() {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/comments")
+      .then((res) => res.json())
+      .then((data) => setComments(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Total comments: {comments.length}</h2>
+      {comments.map((comment) => (
+        <Comment name={comment.name} body={comment.body}></Comment>
+      ))}
+    </div>
+  );
+}
+
+// Comment component
+function Comment(props) {
+  return (
+    <div>
+      <h5>Name: {props.name}</h5>
+      <p>{props.body}</p>
+    </div>
+  );
+}
 export default App;
